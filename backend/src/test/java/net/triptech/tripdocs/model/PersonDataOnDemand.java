7995d7753b2a7b6ae6c84e22a32c11a435045021
David Harrison <david.harrison@stress-free.co.nz>
2013-08-18 22:23:09 +1200

    Removed the old Roo scaffold site in preparation for CodeIgniter based version.

--- backend/src/test/java/net/triptech/tripdocs/model/PersonDataOnDemand.java
+++
@@ -1,7 +0,0 @@
-package net.triptech.tripdocs.model;
-
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-
-@RooDataOnDemand(entity = Person.class)
-public class PersonDataOnDemand {
-}
--- backend/src/test/java/net/triptech/tripdocs/model/PreferencesDataOnDemand.java
+++
@@ -1,7 +0,0 @@
-package net.triptech.tripdocs.model;
-
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-
-@RooDataOnDemand(entity = Preferences.class)
-public class PreferencesDataOnDemand {
-}
