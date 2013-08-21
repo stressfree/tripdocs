14e52ee04320d1236156ea9a3ce7365457d5685a
David Harrison <david.harrison@stress-free.co.nz>
2013-08-22 00:06:57 +1200

    Various tweaks on path to cleaning out code.

--- backend/application/views/footer.php
+++ backend/application/views/footer.php
@@ -1,3 +1,4 @@
-<div class="footer">
-    
+            </div>
+        </div>
+    </div>
 </div>
--- backend/application/views/head.php
+++ backend/application/views/head.php
@@ -1,29 +1,21 @@
-<!DOCTYPE html>
-<html>
-    <head>
-    
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    
+<!--[if IE]><![endif]-->
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!--[if lt IE 7 ]><html class="ie ie6" xmlns="http://www.w3.org/1999/xhtml" lang="en-NZ"><![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" xmlns="http://www.w3.org/1999/xhtml" lang="en-NZ"><![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" xmlns="http://www.w3.org/1999/xhtml" lang="en-NZ"><![endif]-->
+<!--[if IE 9 ]><html class="ie9" xmlns="http://www.w3.org/1999/xhtml" lang="en-NZ"><![endif]-->
+<!--[if gt IE 9]><!-->
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en-NZ"><!--<![endif]-->
+<head>
     <title><?php echo isset($title) ? $title.' - '.lang('website_title') : lang('website_title'); ?></title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="A3M">
-    
-    <base href="<?php echo base_url(); ?>"/>
-    
-    <link rel="shortcut icon" href="<?php echo base_url(); ?>favicon.ico"/>
-    <link type="text/css" rel="stylesheet" href="<?php echo base_url().RES_DIR; ?>/bootstrap/css/bootstrap.min.css"/>
-    <link type="text/css" rel="stylesheet" href="<?php echo base_url().RES_DIR; ?>/css/style.css"/>
-    
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
-    <link href="<?php echo base_url().RES_DIR; ?>/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
-    <script src="<?php echo base_url().RES_DIR; ?>/bootstrap/js/bootstrap.min.js"></script>
-    
-    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-    <!--[if lt IE 9]>
-      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
-    
-    </head>
-        <body>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta content="<?php echo lang('website_copyright'); ?>" name="copyright">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="<?php echo base_url() ?>favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo base_url().RES_DIR; ?>/img/apple-touch-icon-114x114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo base_url().RES_DIR; ?>/img/apple-touch-icon-72x72-precomposed.png">  
+    <link rel="apple-touch-icon-precomposed" href="<?php echo base_url().RES_DIR; ?>/img/apple-touch-icon-57x57-precomposed.png">
+    <link rel="stylesheet" href="<?php echo base_url().RES_DIR; ?>/css/styles.css" type="text/css" />
+</head>
+<body>
